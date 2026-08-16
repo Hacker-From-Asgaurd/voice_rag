82ba30fda6011f7e8afbb0ec509ac1c8747cd241
Darshan Parikh <kanhakanha1202@gmail.com>
2026-08-17 02:18:27 +0530

    Fix syntax error on CORS middleware in app/main.py

--- app/main.py
+++ app/main.py
@@ -81,10 +81,14 @@ app.add_middleware(
         "http://127.0.0.1:8000",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+
 def get_active_voice_pipeline() -> Optional[VoiceRAGPipeline]:
     global voice_pipeline
     if voice_pipeline is not None:
